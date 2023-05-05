e28a2f5836c847ff67b4482aee3934ef513b0c3e
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-05-05 16:10:02 +0500

    edit code based on review

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :users
-  belongs_to :posts
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
   after_save :update_likes_counter
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   def self.three_recent_posts
     Post.order(created_at: :desc).limit(3)
