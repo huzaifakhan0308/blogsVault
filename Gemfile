2ff247b09ffeafab28665ee92216104f83b9b179
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-05-18 21:22:38 +0500

    Add devise and set my code for website sign in/sign up

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ group :test do
 end
 
 gem 'rails-controller-testing'
+
+gem 'devise'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.all
   end
 
   def show
+    if params[:id] == 'sign_out'
+      sign_out_and_redirect
+    else
+      find_user
+    end
+  end
+
+  def sign_out_and_redirect
+    sign_out current_user
+    redirect_to root_path
+  end
+
+  def find_user
     @user = User.find(params[:id])
     @posts = @user.three_recent_posts
   end
