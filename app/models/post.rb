baa1a6c41919a061816daa4eb01dcc919759d0bb
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-05-19 15:27:41 +0500

    add authorization

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,13 +6,20 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
   after_save :update_posts_counter
+
+  after_destroy :decrement_user_post_counter
 
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
   private
+
+  def decrement_user_post_counter
+    author.decrement!(:postscounter)
+  end
 
   def update_posts_counter
     author.update(postscounter: author.posts.count)
